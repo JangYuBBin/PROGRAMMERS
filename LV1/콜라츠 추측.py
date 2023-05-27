becc453520b11a36d67acfff28a8dfbe2f368a53
JangYuBBin <129468179+JangYuBBin@users.noreply.github.com>
2023-05-27 22:47:21 +0900

    Create 콜라츠 추측.py

---
+++ LV1/콜라츠 추측.py
@@ -0,0 +1,18 @@
+# 콜라츠 추측
+
+def solution(num):
+    answer = 0
+    
+    while num != 1 and answer < 500:
+        if num % 2 == 0:
+            num //= 2
+            answer += 1
+        else:
+            num *= 3
+            num += 1
+            answer += 1
+    
+    if num == 1:
+        return answer
+    else:
+        return -1
